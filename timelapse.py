31ce6036d5d817598f4cc58ee7c2de2c191eba41
Marco Stixenberger <marco@marco-y50>
2020-04-05 19:08:30 +0200

    Update timelapse.py

--- timelapse.py
+++ timelapse.py
@@ -3,7 +3,11 @@ import picamera
 
 delay = 15
 
+photocount = 0
+
 with picamera.PiCamera() as camera:
     camera.resolution = (1024, 768)
     for filename in camera.capture_continuous('pics/img{timestamp:%H-%M-%S-%f}.jpg'):
+        photocount = photocount + 1
+        print('Number of Pics: {}'.format(photocount))
         sleep(delay)
